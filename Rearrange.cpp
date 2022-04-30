9573d620eb4b868d17ef557a29f8a0a6841bf694
Thanh Toàn <acemarco9@gmail.com>
2022-04-30 20:30:55 +0700

    done[32;2u

--- Rearrange.cpp
+++ Rearrange.cpp
@@ -4,7 +4,15 @@
 
 Individual Rearrange::mutate(Individual dna, int k){ 
     if (k <= 0) return dna; 
-    int pos = (k-1) % dna.getLength(); 
+    if (k - 1 < dna.getLength()){ 
+        std::string start = dna.getString().substr(k-1); 
+        std::string end = dna.getString().substr(0, k-1); 
+        std::string mutated = start + end; 
+        Individual copy = Individual(mutated); 
+        return copy; 
+    }
+    int pos = k % dna.getLength(); 
+    pos -= 1; 
     std::string start = dna.getString().substr(pos); 
     std::string end = dna.getString().substr(0, pos); 
     std::string mutated = start + end; 
